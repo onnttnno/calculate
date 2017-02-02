7f7de2b97004fdb3bdac79ea90b9c228f2fddac4
Jirawat Phattarawasitpol <plamjirawat@hotmail.com>
2017-02-02 20:28:24 +0700

    calculate version 2.0 By Jirawat
    Add feature Squareroot and Power.

--- calculate/src/calculate/calculateCore.java
+++ calculate/src/calculate/calculateCore.java
@@ -1,21 +1,22 @@
 package calculate;
 
 import java.util.Scanner;
+import java.lang.*;
 
 public class calculateCore {
 
 	private static double numOne;
 	private static double numTwo;
 	private static int mode;
-	
+
 	private static Handerler handy=null;
 	private static calculateCore instance = null;
-	
+
 	public calculateCore() {
 		handy = new Handerler();
-		
+
 	}
-	
+
 	public static calculateCore getInstance() {
 		   if(instance == null) {
 		      instance = new calculateCore();
@@ -23,56 +24,56 @@ public class calculateCore {
 		   return instance;
 		}
 
-	
-	
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//display show
 		if(handy== null){
 			handy = new Handerler();
-			
+
 		}
 		Display display = new Display();
 		display.show();
-		
+
 		String x="";
 		Scanner sc = new Scanner(System.in);
-		
+
 		do{
 			System.out.println("ENTER YOUR CHOICE: ");
 			x = sc.next();
-			
+
 		}while( handy.checkMode(x) );
-		
+
 		//mode
 		mode=Integer.parseInt(x);
-		
-		
+
+
 		//num1
 		String y="";
 		do{
 			System.out.println("ENTER Number 1: ");
 			y = sc.next();
-			
+
 		}while( handy.checkDouble(y) );
 		numOne = Double.parseDouble(y);
-		
-		
+
+
 		//num2
 		String z="";
 		do{
-			System.out.println("ENTER Number 2: ");
+			System.out.println("ENTER Number 2 (type 0 if you need to calculate Squareroot): ");
 			z = sc.next();
-			
+
 		}while( handy.checkDouble(z) );
 		numTwo = Double.parseDouble(z);
-		
-		
+
+
 		double result = compute(numOne , numTwo,mode);
-		
-		
+
+
 		System.out.println("RESULT = "+result);
-		
+
 	}
 
 	private static double compute(double numOne, double numTwo, int mode) {
@@ -83,7 +84,7 @@ public class calculateCore {
             case 1:
             	SUM sum = new SUM();
             	result = sum.compute(numOne,numTwo);
-               
+
                 break;
             case 2:
             	SUBTRACTION subtraction = new SUBTRACTION();
@@ -111,18 +112,18 @@ public class calculateCore {
             	do{
             	System.out.println("YOU WILL BE ADD MORE NUMBER ?(PRESS Y/N)");
             	anw = scan.next();
-            	
+
             	if(anw.equals("Y")){
             		String z="";
             		do{
             			System.out.println("ENTER Number : ");
             			z = scan.next();
-            			
+
             		}while( handy.checkDouble(z) );
             		double num = Double.parseDouble(z);
             		count++;
             		result = result +num;
-            		
+
             	}
             	else if(anw.equals("N")){
             		break;
@@ -131,22 +132,33 @@ public class calculateCore {
             		System.out.println("YOU HAVE ENTERED A WRONG ");
             		break;
             	}
-            	
+
             	}while(anw.equals("Y"));
             	result= result/count;
-            	
-            	
-            	
+						//๋Jirawat Phattarawasitpol @ 20:14 PM, 2 FEB 2017//
             case 6:
-            	
-            	result = Math.pow(numOne,numTwo);
+							POWER pow = new POWER();
+							result = pow.compute(numOne,numTwo);
             	break;
+						case 7:
+							SQUAREROOT squareroot = new SQUAREROOT();
+							if (numTwo == 0) {
+								if (numOne < 0) {
+									result = squareroot.compute(numOne,numTwo);
+								} else {
+										System.out.println("YOU CAN'T INPUT NEGATIVE NUMBER.");
+								}
+							} else {
+								System.out.println("YOU HAVE ENTERED SOMETHING WRONG.");
+							}
+            	break;
+						//===============================================//
             default:
                 System.out.println("YOU HAVE ENTERED A WRONG CHOICE");
-      
+
         }
 
-        
+
 		return result;
 	}
 
